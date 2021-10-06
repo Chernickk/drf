807b83503504ebd9970396a22501443025d934db
Nik <17rolex@gmail.com>
2021-10-06 18:00:49 +0300

    lesson_6

--- todo/urls.py
+++ todo/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from authapp.views import UserViewSet
 from todoapp.views import ProjectViewSet, TodoViewSet
 
@@ -28,5 +31,9 @@ router.register('todos', TodoViewSet, basename='todos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
